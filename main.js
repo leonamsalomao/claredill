280ad339f5fb5b28b1cf75a3148847b1bd89fb97
leo_salomao7 <70729829+leonamsalomao@users.noreply.github.com>
2022-11-23 15:54:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 /* Barra superior */
 
-const navigation = document.querySelector('#navigation')
-console.log(navigation)
+window.addEventListener('scroll', onScroll)
 
+const navigation = document.querySelector('#navigation')
+const backToTopButton = document.querySelector('#backToTopButton')
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 400) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
   }
 }
 
@@ -20,3 +36,13 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+/* ScrollReveal */
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}).reveal(
+  '#home, #home Image, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content'
+)
