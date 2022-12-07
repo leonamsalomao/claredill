3ce230a450ab9ba6c0817f078636990c36c9a699
leo_salomao7 <70729829+leonamsalomao@users.noreply.github.com>
2022-12-07 13:48:01 -0300

    Pronto

--- main.js
+++ main.js
@@ -10,12 +10,44 @@ function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
 
-  activateMenuAtCurrentSection()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(product)
+  activateMenuAtCurrentSection(about)
 }
 
-function activateMenuAtCurrentSection() {
+function activateMenuAtCurrentSection(section) {
   const targetLine = scrollY + innerHeight / 2
+
+  /* Verifica se a seção passou da linha */
+
+  //Topo da seção
+  const sectionTop = section.offsetTop
+
+  //Altura da seção
+  const sectionHeight = section.offsetHeight
+
+  //Topo da seção chegou ou ultrapassou a linha alto
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
 }
+
+
 
 function showNavOnScroll() {
   if (scrollY > 0) {
@@ -50,5 +82,5 @@ ScrollReveal({
   distance: '30px',
   duration: 700
 }).reveal(
-  '#home, #home Image, #home .stats, #services, #services header, #services .card, #about, #about header, #about .content'
+  '#home, #home Image, #home .stats, #product header, #product header img, #product .card, #about, #about header, #about .content'
 )
